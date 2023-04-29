6c1afd0c78413151de5d468b56617985bc2f559b
akast123 <a.kapoor@iitg.ac.in>
2023-04-29 15:41:53 +0530

    added upto past perfromance

--- hello.py
+++ hello.py
@@ -58,19 +58,19 @@ for i in range(len(dum_list_pota['deliveries'])):
 #     # print(i,' ',bowldata[i])
         
 # # CURRENT MATCH    
-# # (general stuff)    
-# 1 bowl number 
-# 2 run rate
-# 3 Required run rate(0 for 1st innings)
-# 4 wicket array
-# # bowler's performance 
-# 5 current economy (runs /balls)
-# 6 wicket performance (total wickets taken)
-# batsmen
-# 7 strike rate(runs / ball * 100)
-# 8 total runs
-# #batsmen vs bowler
-# 9 balls/runs
+# # (general stuff)   ✅ 
+# 1 bowl number ✅
+# 2 run rate ✅
+# 3 Required run rate(0 for 1st innings) ✅
+# 4 wicket array ✅
+# # bowler's performance ✅
+# 5 current economy (runs /balls)✅
+# 6 wicket performance (total wickets taken)✅
+# batsmen✅
+# 7 strike rate(runs / ball * 100)✅
+# 8 total runs✅
+# #batsmen vs bowler✅
+# 9 runs/balls✅
 
 # #PREVIOUS DATA
 
@@ -97,29 +97,86 @@ vec = np.zeros([124,24])
 wickets = np.zeros(10)
 wicketct = 0
 wickets[0] = 1
+bowlerperf = {}
+dumdictb = {}
+batsmanperf = {}
+for i in range(len(dum_list_pota['deliveries'])):
+    for key , value in dum_list_pota['deliveries'][i].items():
+        bowlername = value['bowler']
+        batsmanname = value['batsman']
+        dumdictb[batsmanname + ' to ' + bowlername] = [0,0]
+        bowlerperf[bowlername] = [0,0,0]
+        batsmanperf[batsmanname] = [0,0]
+
+
+# print(bowlerperf)
 
 for i in range(len(dum_list_pota['deliveries'])):
     for key , value in dum_list_pota['deliveries'][i].items():
-        print(key,' ',value)
+        # print(key,' ',value)
         bowlnum += 1
         totruns += value['runs']['total']
         vec[bowlnum - 1][0] = bowlnum
+
         vec[bowlnum - 1][1] = totruns/bowlnum
+
         vec[bowlnum - 1][2] = (target - totruns)/(125 - bowlnum)*(innings)
+
+        
+        flg = 0
+
         if 'wicket' in value:
             wickets[wicketct] = 0
             wicketct += 1
+            flg = 1
             wickets[wicketct] = 1
-        
+
         for j in range(10):
             vec[bowlnum-1][3+j] = wickets[j]
+
+        bowlername = value['bowler']
+        batsmanname = value['batsman']
+
+        bowlerperf[bowlername][0] += value['runs']['total']
+        bowlerperf[bowlername][1] += flg
+        bowlerperf[bowlername][2] += 1
+        
+        dumdictb[batsmanname + ' to ' + bowlername][0] += value['runs']['total']
+        dumdictb[batsmanname + ' to ' + bowlername][1] += 1
+        
+        # current economy (runs /balls)
+        vec[bowlnum - 1][13] = bowlerperf[bowlername][0]/bowlerperf[bowlername][2]
+
+        # wicket performance (total wickets taken)
+        vec[bowlnum - 1][14] = bowlerperf[bowlername][1]
+        
+
+        batsmanperf[batsmanname][0] += value['runs']['total']
+        batsmanperf[batsmanname][1] += 1
+
+        # strike rate(runs / ball * 100)
+        vec[bowlnum - 1][15] = (batsmanperf[batsmanname][0]/batsmanperf[batsmanname][1])*100.00
+        
+        # total runs
+        vec[bowlnum - 1][16] = batsmanperf[batsmanname][0]
+
+        # runs/balls
+        if dumdictb[batsmanname + ' to ' + bowlername][0] != 0:
+            vec[bowlnum - 1][17] = dumdictb[batsmanname + ' to ' + bowlername][0]/dumdictb[batsmanname + ' to ' + bowlername][0]
+
+
+
+
+        
+
+        
         
         
         
         
         
 
-print(bowlnum)
+
 
 
 
@@ -131,6 +188,7 @@ print(bowlnum)
 
 
 
-
+for i in vec:
+    print(i)
 
     
